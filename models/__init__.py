bab384b6637becdc62afcd8740c906255148eae7
Meryem <dobli.meryeme@gmail.com>
2023-08-22 14:06:07 -0700

    update cities and state

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
+storage_type = getenv('HBNB_TYPE_SORAGE')
 
 storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models import storage; storage_type
 
-
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+
+    name = Column(String(128), nullable=False)
+
+    if storage_type == 'db':
+        cities = relationship("City", backref="state", cascade="all, delete-orphan")
+
+    else:
+        
+        @property
+        def cities(self):
+            city_instances = storage.all(City)
+            return [city for city in city_instances.values() if city.state_id == self.id]
+            
