366cd63d70f9b0df2e245a1f1a1942cc4afac2b7
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-22 15:33:23 +0100

    t

--- models/base_model.py
+++ models/base_model.py
@@ -15,8 +15,10 @@ class BaseModel:
             self.updated_at = datetime.now()
         else:
             self.id = kwargs.get('id', str(uuid.uuid4()))
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
 
             del kwargs['__class__']
             self.__dict__.update(kwargs)
