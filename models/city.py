2f636a2f10925bb00474603b948d16f10f1edbc2
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-23 14:34:19 +0000

    task 8

--- models/city.py
+++ models/city.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
+from models import storage_type
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
@@ -11,5 +11,5 @@ class City(BaseModel, Base):
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = relationship('Place', backref='cities',
-                              cascade='all, delete, delete-orphan')
+    if storage_type == "db":
+        places = relationship('Place', backref='cities', cascade='all, delete, delete-orphan')
--- models/user.py
+++ models/user.py
@@ -2,7 +2,8 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
@@ -12,3 +13,5 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+    if storage_type == "db":
+        places = relationship('Place', backref='users', cascade='all, delete, delete-orphan')
