012f3747b1d5651d3808146a853d6e16a714715b
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-08-23 16:17:15 +0000

    task 9

--- models/review.py
+++ models/review.py
@@ -7,5 +7,5 @@ class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
-    palce_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
