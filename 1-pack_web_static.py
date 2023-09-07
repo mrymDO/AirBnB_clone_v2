c6c94bde4f4fac0747f3ded6abf0cb480f9b42bd
Meryem <dobli.meryeme@gmail.com>
2023-09-07 01:40:18 -0700

    Generates a .tgz archive from the web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" generate a .tgz archive from contents of web_static """
+
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """ generate a tar.gz file from web_static folder """
+    
+    local("mkdir -p versions")
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    path = ("versions/web_static_{}.tgz".format(timestamp))
+    result = local("tar -czvf {} web_static".format(path))
+
+    if result.failed:
+        return None
+    return path
