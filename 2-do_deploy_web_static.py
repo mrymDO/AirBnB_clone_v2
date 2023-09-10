05a7e2a330495d41eb62781a0df72e39a4dcf742
Meryem <dobli.meryeme@gmail.com>
2023-09-10 12:38:14 -0700

    fix1

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,30 +20,6 @@ def do_deploy(archive_path):
         folder_name = file_name.replace(".tgz", "")
         folder_path = "/data/web_static/releases/{}/".format(folder_name)
 
-        # Upload the archive to /tmp/ directory on the server
-        put(archive_path, "/tmp/")
-
-        # Create necessary directories and extract the archive
-        run("mkdir -p {}".format(folder_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-
-        # Remove temporary archive file
-        run("rm -rf /tmp/{}".format(file_name))
-
-        # Move the extracted files to the appropriate location
-        run("mv {}/web_static/* {}".format(folder_path, folder_path))
-
-        # Remove the web_static directory within the release folder
-        run("rm -rf {}/web_static".format(folder_path))
-
-        # Remove the old symbolic link
-        run("rm -rf /data/web_static/current")
-
-        # Create a new symbolic link pointing to the new version
-        run("ln -s {} /data/web_static/current".format(folder_path))
-
-        print('deployment done')
-
         # Check if the code should run locally
         run_locally = os.getenv("run_locally", None)
         if run_locally is not None:
@@ -54,7 +30,27 @@ def do_deploy(archive_path):
             local("rm -rf {}/web_static".format(folder_path))
             local("rm -rf /data/web_static/current")
             local("ln -s {} /data/web_static/current".format(folder_path))
+        else:
+            # Upload the archive to /tmp/ directory on the server
+            put(archive_path, "/tmp/")
 
+            # Create necessary directories and extract the archive
+            run("mkdir -p {}".format(folder_path))
+            run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+            # Remove temporary archive file
+            run("rm -rf /tmp/{}".format(file_name))
+
+            # Move the extracted files to the appropriate location
+            run("mv {}/web_static/* {}".format(folder_path, folder_path))
+            # Remove the web_static directory within the release folder
+            run("rm -rf {}/web_static".format(folder_path))
+
+            # Remove the old symbolic link
+            run("rm -rf /data/web_static/current")
+
+            # Create a new symbolic link pointing to the new version
+            run("ln -s {} /data/web_static/current".format(folder_path))
+            print('deployment done')
         return True
 
     except Exception as e:
