c5709aeb6fc182f1892ccfd70cc1e53826600aef
Meryem <dobli.meryeme@gmail.com>
2023-09-10 13:34:34 -0700

    fix3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """ Fabric script to distribute an archive to web servers """
 
-from fabric.api import env, run, put, sudo, local
+from fabric.api import env, run, put, sudo
 from os.path import exists
-import os
 
 env.hosts = ['52.86.193.42', '52.86.161.66']
 
@@ -20,37 +19,29 @@ def do_deploy(archive_path):
         folder_name = file_name.replace(".tgz", "")
         folder_path = "/data/web_static/releases/{}/".format(folder_name)
 
-        # Check if the code should run locally
-        run_locally = os.getenv("run_locally", None)
-        if run_locally is not None:
-            local("mkdir -p {}".format(folder_path))
-            local("tar -xzf {} -C {}".format(archive_path, folder_path))
-            local("rm -rf /tmp/{}".format(file_name))
-            local("mv {}/web_static/* {}".format(folder_path, folder_path))
-            local("rm -rf {}/web_static".format(folder_path))
-            local("rm -rf /data/web_static/current")
-            local("ln -s {} /data/web_static/current".format(folder_path))
-        else:
-            # Upload the archive to /tmp/ directory on the server
-            put(archive_path, "/tmp/")
+        # Upload the archive to /tmp/ directory on the server
+        put(archive_path, "/tmp/")
 
-            # Create necessary directories and extract the archive
-            run("mkdir -p {}".format(folder_path))
-            run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
-            # Remove temporary archive file
-            run("rm -rf /tmp/{}".format(file_name))
+        # Create necessary directories and extract the archive
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
 
-            # Move the extracted files to the appropriate location
-            run("mv {}/web_static/* {}".format(folder_path, folder_path))
-            # Remove the web_static directory within the release folder
-            run("rm -rf {}/web_static".format(folder_path))
+        # Remove temporary archive file
+        run("rm -rf /tmp/{}".format(file_name))
 
-            # Remove the old symbolic link
-            run("rm -rf /data/web_static/current")
+        # Move the extracted files to the appropriate location
+        run("mv {}/web_static/* {}".format(folder_path, folder_path))
 
-            # Create a new symbolic link pointing to the new version
-            run("ln -s {} /data/web_static/current".format(folder_path))
-            print('deployment done')
+        # Remove the web_static directory within the release folder
+        run("rm -rf {}/web_static".format(folder_path))
+
+        # Remove the old symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link pointing to the new version
+        run("ln -s {} /data/web_static/current".format(folder_path))
+
+        print('deployment done')
         return True
 
     except Exception as e:
